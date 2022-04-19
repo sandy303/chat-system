d2a2fc0dc4cb162f4671998110786eed1a4b24d0
sandyNader <sandytawfik30@gmail.com>
2022-04-20 00:29:56 +0200

    Fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+
+  resources :conversations do
+    resources :messages
+  end
+
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "convesrations#index"
 end
